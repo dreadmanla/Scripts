aed4ee64be783c97f4243843938498c764af9c46
dreadmanla <118125452+dreadmanla@users.noreply.github.com>
2023-02-07 21:19:34 +0100

    Update for crucifix.lua

--- for crucifix.lua
+++ for crucifix.lua
@@ -1,7 +1,10 @@
 for i,v in pairs(workspace.Repentance.Pentagram:GetChildren()) do
 local Bcs = workspace.Repentance
+	if not v:IsA("Beam") then
+		return
+	end
 Bcs.BeamTransparency.Changed:Connect(function()
-	script.Parent.Transparency = NumberSequence.new(Bcs.BeamTransparency.Value,Bcs.BeamTransparency.Value)
+	v.Transparency = NumberSequence.new(Bcs.BeamTransparency.Value,Bcs.BeamTransparency.Value)
 end)
 
 end
